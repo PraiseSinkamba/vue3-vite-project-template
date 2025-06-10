d5e6f0b3ae89020e54b4bcd521bd2e852f0b8288
Malumbo Sinkamba <malumbosink@gmail.com>
2025-06-10 13:33:36 +0300

    Deleted boilerplate components and added pinia colada

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import { PiniaColadaDevtools } from '@pinia/colada-devtools'
 </script>
 
 <template>
   <RouterView />
+  <PiniaColadaDevtools />
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './assets/main.css'
 import App from './App.vue'
 import { Button } from '@/components/ui/button'
 import { createPinia } from 'pinia'
+import {PiniaColada} from '@pinia/colada'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import router from './router'
 import { createApp } from 'vue'
@@ -10,6 +11,13 @@ import { createApp } from 'vue'
 const app = createApp(App)
 const pinia = createPinia().use(piniaPluginPersistedstate)
 
-app.use(pinia).use(router).component('Button', Button)
+app.use(pinia)
+  .use(PiniaColada,{
+    queryOptions :{
+      gcTime: (1000 * 60/*1min*/) * 5 //5min
+    }
+  })
+  .use(router)
+  .component('Button', Button)
 
 app.mount('#app')
